944bc584934aa4a752bb321297d0703983c31d70
Fabiano <fabiano.ssantos79@gmail.com>
2023-05-18 01:27:34 -0300

    init project

---
+++ src/pages/Cardapio/index.tsx
@@ -0,0 +1,13 @@
+import styles from './Cardapio.module.scss'
+import {ReactComponent as Logo} from 'assets/logo.svg'
+
+export default function Cardapio() {
+  return (
+    <main>
+        <nav className={styles.menu}>
+            <Logo />
+            <h1>ITALIANA</h1>
+        </nav>
+    </main>
+  )
+}
