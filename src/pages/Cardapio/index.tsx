acee412214e8ac33b13d1bd48e8c96e4d6dbb477
Fabiano <fabiano.ssantos79@gmail.com>
2023-05-18 23:46:36 -0300

    update system

--- src/pages/Cardapio/index.tsx
+++ src/pages/Cardapio/index.tsx
@@ -2,9 +2,11 @@ import {useState} from 'react'
 import Buscador from './Buscador'
 import styles from './Cardapio.module.scss'
 import {ReactComponent as Logo} from 'assets/logo.svg'
+import Filtros from './Filtros'
 
 export default function Cardapio() {
-    const [busca, setBusca] = useState('') 
+    const [busca, setBusca] = useState('');
+    const [filtro, setFiltro] = useState<number | null>(null);
 
   return (
     <main>
@@ -21,7 +23,11 @@ export default function Cardapio() {
                 Cardápio
             </h3>
             <Buscador busca={busca} setBusca={setBusca}/>
+            <div className={styles.cardapio__filtros}>
+            <Filtros filtro={filtro} setFiltro={setFiltro} />
+        </div>
         </section>
+        
     </main>
   )
 }
